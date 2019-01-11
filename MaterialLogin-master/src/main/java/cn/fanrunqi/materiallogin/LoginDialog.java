58abd0408374af1b575732fe0a9ecf95c855a6cb
shenkun <825210484@qq.com>
2019-01-11 17:33:55 +0800

    动画修改

--- MaterialLogin-master/src/main/java/cn/fanrunqi/materiallogin/LoginDialog.java
+++ MaterialLogin-master/src/main/java/cn/fanrunqi/materiallogin/LoginDialog.java
@@ -2,9 +2,11 @@ package cn.fanrunqi.materiallogin;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
@@ -20,6 +22,8 @@ public class LoginDialog extends Dialog {
     private CardView mCv;
     private CardView mCv_loading;
     private Button mBt_go;
+    private float mScaleX;
+    private float mScaleY;
 
     LoginDialog(Context context) {
         super(context, R.style.BaseDialogStyle);
@@ -36,20 +40,58 @@ public class LoginDialog extends Dialog {
         mCv = mRootView.findViewById(R.id.cv);
         mCv_loading = mRootView.findViewById(R.id.cv_loading);
 
+
         mBt_go = mRootView.findViewById(R.id.bt_go);
+
 
         mBt_go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCv.animate().scaleX(0.7f)
-                        .scaleY(0.7f)
+                mCv.animate().scaleX(0.5f)
+                        .scaleY(0.5f)
                         .setDuration(500)
                         .setInterpolator(new AccelerateDecelerateInterpolator())
+                        .start();
+//                mCv.animate().alpha(0)
+
+//                        .setDuration(800)
+//                        .start();
+                mCv_loading.animate().alpha(1)
+                        .setDuration(300)
                         .start();
                 mHandler.sendEmptyMessageDelayed(1, 500);
             }
         });
+
+        //缩放动画啊
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        int loadheight = mCv_loading.getMeasuredHeight();
+        int loadwidth = mCv_loading.getMeasuredWidth();
+
+        int height = mCv.getMeasuredHeight();
+        int width = mCv.getMeasuredWidth();
+
+
+        //宽缩放
+        mScaleX = (float) loadwidth / width;
+        mScaleY = (float) loadheight / height;
+
+        Log.e("====", "mScaleX: " + mScaleX + "mScaleY:" + mScaleY);
+    }
+
 
     Handler mHandler = new Handler() {
         @Override
@@ -57,9 +99,11 @@ public class LoginDialog extends Dialog {
 //            mView1.setVisibility(View.GONE);
 //            mView2.setVisibility(View.VISIBLE);
 //            mView1.setBackgroundResource(R.color.dialog_cancel);
-            mCv.setVisibility(View.INVISIBLE);
-            mCv_loading.setVisibility(View.VISIBLE);
+//            mCv.setVisibility(View.INVISIBLE);
 
+            mCv.setAlpha(0);
+//            mCv_loading.setVisibility(View.VISIBLE);
+//            mCv_loading.setAlpha(1);
         }
     };
 
